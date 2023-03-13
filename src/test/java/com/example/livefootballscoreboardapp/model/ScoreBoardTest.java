afdebcaf092abdca80961b6f01c6582347a4fabd
k.tukhtasinov <k.tukhtasinov@fdmgroup.com>
2023-03-13 18:04:33 +0100

    Fourth Test "testGetGames" is done and passed

--- src/test/java/com/example/livefootballscoreboardapp/model/ScoreBoardTest.java
+++ src/test/java/com/example/livefootballscoreboardapp/model/ScoreBoardTest.java
@@ -34,4 +34,28 @@ public class ScoreBoardTest {
         sb.endGame("Poland", "Holland");
         Assertions.assertEquals(0, sb.getGames().size());
     }
+
+    @Test
+    public void testGetGames() {
+        sb.addNewGame("Poland", "Holland");
+        sb.addNewGame("France", "Italy");
+        sb.addNewGame("Portugal", "Spain");
+
+        sb.updateScore("Poland", "Holland", 2, 1);
+        sb.updateScore("France", "Italy", 1, 1);
+        sb.updateScore("Portugal", "Spain", 0, 0);
+
+        sb.endGame("France", "Italy");
+
+        Assertions.assertEquals("Poland", sb.getGames().get(0).getHomeTeam());
+        Assertions.assertEquals("Holland", sb.getGames().get(0).getAwayTeam());
+        Assertions.assertEquals(2, sb.getGames().get(0).getHomeScore());
+        Assertions.assertEquals(1, sb.getGames().get(0).getAwayScore());
+
+        Assertions.assertEquals("Portugal", sb.getGames().get(1).getHomeTeam());
+        Assertions.assertEquals("Spain", sb.getGames().get(1).getAwayTeam());
+        Assertions.assertEquals(0, sb.getGames().get(1).getHomeScore());
+        Assertions.assertEquals(0, sb.getGames().get(1).getAwayScore());
+
+    }
 }
