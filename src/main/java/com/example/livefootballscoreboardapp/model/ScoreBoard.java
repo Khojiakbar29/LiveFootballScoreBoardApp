f0f6d3c7414094e09acb1eda360189ea9408efef
k.tukhtasinov <k.tukhtasinov@fdmgroup.com>
2023-03-13 17:36:17 +0100

    ScoreBoard class is ready

--- src/main/java/com/example/livefootballscoreboardapp/model/ScoreBoard.java
+++ src/main/java/com/example/livefootballscoreboardapp/model/ScoreBoard.java
@@ -1,21 +1,56 @@
 package com.example.livefootballscoreboardapp.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ScoreBoard {
-    private List<Game> gamesInProgress;
+    private List<Game> activeGames;
 
     public ScoreBoard() {
-        gamesInProgress = new ArrayList<>();
+        activeGames = new ArrayList<>();
     }
 
     public void addNewGame(String homeTeam, String awayTeam) {
         Game g = new Game(homeTeam, awayTeam);
-        gamesInProgress.add(g);
+        activeGames.add(g);
     }
 
     public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
         Game g = findGame(homeTeam, awayTeam);
+        g.setHomeScore(homeScore);
+        g.setAwayScore(awayScore);
+    }
+
+    public void endGame(String homeTeam, String awayTeam) {
+        Game g = findGame(homeTeam, awayTeam);
+        activeGames.remove(g);
+    }
+
+    public List<Game> getGames() {
+        List<Game> sum = new ArrayList<>(activeGames);
+
+        Collections.sort(sum, new Comparator <Game>() {
+
+            @Override
+            public int compare(Game o1, Game o2) {
+                int score1 = o1.getHomeScore() + o1.getAwayScore();
+                int score2 = o2.getHomeScore() + o2.getAwayScore();
+
+                if (score1 != score2) {
+                    return score2 - score1;
+                } else {
+                    return o2.getStartTime().compareTo(o1.getStartTime());
+                }
+            }
+        });
+        return sum;
+    }
+
+    private Game findGame(String homeTeam, String awayTeam) {
+        for (Game g : activeGames) {
+            if (g.getHomeTeam().equals(homeTeam) && g.getAwayTeam().equals(awayTeam)) {
+                return g;
+            }
+        }
+        throw new RuntimeException("There is no requested game!");
     }
 }
